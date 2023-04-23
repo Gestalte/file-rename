490870d5f9734ddf734367d3cffa73f7d289a8e2
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-04-23 14:30:58 +0200

    Add a way to handle exceptions to title case.

--- file-rename.py
+++ file-rename.py
@@ -1,6 +1,22 @@
 import os
 import re 
 
+# TODO: Handle editions i.e 1st, 2nd etc. so that they don't come out as 1St, 2Nd etc.
+# TODO: Accept a list of acronyms/exceptions that should conform to a specific format. i.e. WPF instead of Wpf and .NET instead of Net
+
+specialCases = [".NET","WPF","GUI","SQL"]
+
+def getSpecialCases(input:str):
+    output = []
+    for case in specialCases:
+        if case == input:
+            output.append(case)
+
+class Renamable:
+    def __init__(self,name):
+        self.beforeName = name
+        self.specialCases = getSpecialCases(name)
+
 def ruleReplaceDash(input:str):
     return input.replace("-"," ")
     
@@ -15,7 +31,8 @@ def ruleReplacePeriod(input:str):
     return  name + "." + extension
 
 def ruleTitleCase(input:str):
-    return input.title()
+    splits = input.split(".")
+    return splits[0].title() + "." + splits[1]
 
 def ruleTrim(input:str):
     return input.strip()
@@ -28,10 +45,29 @@ rules = [
     ruleTrim
 ]
 
+def saveSpecialCases(input:str):
+    output = []
+    count = 0
+    input = input.lower()
+    for case in specialCases:
+        if input.find(case.lower()) != -1:
+            placeholder = "§" + str(count)
+            count = count + 1
+            input = input.replace(case.lower(),placeholder)
+            output.append((placeholder,case))
+    print("input",input)
+    print("output",output)
+    return (input, output)
+
 def renameFile(inputName:str):
-    newName:str = inputName
+    print("inputName",inputName)
+    specials = saveSpecialCases(inputName)
+    newName:str = str(specials[0])
     for rule in rules:
         newName = rule(newName)
+        print("newName",newName)
+    for special in specials[1]:
+        newName = newName.replace(special[0],special[1])
         print("newName",newName)
     #os.rename(inputName, newName)
 
