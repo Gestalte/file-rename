041d800d4c018cf78ac6bd1a068ccb4e13059ddd
Gestalte <66849891+Gestalte@users.noreply.github.com>
2023-08-08 10:23:33 +0200

    Apply Flake8 suggestions.

--- @file-rename.py
+++ @file-rename.py
@@ -1,51 +1,61 @@
 import os
-import re 
+import re
 
-# List of acronyms/exceptions that should conform to a specific format. 
+# List of acronyms/exceptions that should
+# conform to a specific format.
 # i.e. WPF instead of Wpf and .NET instead of Net
-# Compound form like ASP.NET has to come before standalone form like ASP and .NET
+# Compound form like ASP.NET has to come before
+# standalone form like ASP and .NET
 specialCases = [
-    "ASP.NET",".NET","ASP","WPF","GUI",
-    "SQL", "SQLite", "JS", "PHP", "IO", 
-    "MVC", "APIs", "API", "AI","HTML5",
+    "ASP.NET", ".NET", "ASP", "WPF", "GUI",
+    "SQL", "SQLite", "JS", "PHP", "IO",
+    "MVC", "APIs", "API", "AI", "HTML5",
     "HTML"
-    ] 
+    ]
 
-suffixesToRemove = ["annas-archive","annas archive"]
-ordinalIndicaters =["st","rd","th"]
+suffixesToRemove = ["annas-archive", "annas archive"]
+ordinalIndicaters = ["st", "rd", "th"]
 
-def ruleReplaceDash(input:str):
-    return input.replace("-"," ")
-    
-def ruleReplaceUnderscore(input:str):
-    return input.replace("_"," ")
 
-def ruleReplacePeriod(input:str):
+def ruleReplaceDash(input: str):
+    return input.replace("-", " ")
+
+
+def ruleReplaceUnderscore(input: str):
+    return input.replace("_", " ")
+
+
+def ruleReplacePeriod(input: str):
     splits = input.split(".")
     nameSplit = splits[0:len(splits)-1]
     extension = splits[len(splits)-1:len(splits)][0]
     name = " ".join(nameSplit)
-    return  f"{name}.{extension}"
+    return f"{name}.{extension}"
 
-def ruleTitleCase(input:str):
+
+def ruleTitleCase(input: str):
     splits = input.split(".")
     return f"{splits[0].title()}.{splits[1]}"
 
-def ruleOrdinalIndicatorCase(input:str):
+
+def ruleOrdinalIndicatorCase(input: str):
     var = input
-    for rTup in re.findall(r"(\d(Nd|Rd|Th))",input):
+    for rTup in re.findall(r"(\d(Nd|Rd|Th))", input):
         var = var.replace(rTup[0], rTup[0].lower())
     return var
 
-def ruleTrim(input:str):
+
+def ruleTrim(input: str):
     return input.strip()
 
-def ruleRemoveSuffixes(input:str):
-    split = tuple(input.rsplit(".",1))
+
+def ruleRemoveSuffixes(input: str):
+    split = tuple(input.rsplit(".", 1))
     val = split[0]
     for suffix in suffixesToRemove:
         val = val.lower().removesuffix(suffix.lower()).rstrip()
     return f"{val}.{split[1]}"
+
 
 rules = [
     ruleReplaceDash,
@@ -57,33 +67,36 @@ rules = [
     ruleTrim
 ]
 
-def saveSpecialCases(input:str):
+
+def saveSpecialCases(input: str):
     output = []
     count = 0
-    split = tuple(input.rsplit(".",1))
+    split = tuple(input.rsplit(".", 1))
     nameOnly = split[0].lower()
     for case in specialCases:
         pattern = f"(^|\\s){case.lower()}(\\s|$)"
         if re.search(pattern, nameOnly):
             placeholder = f"§{str(count)}"
             count = count + 1
-            nameOnly = nameOnly.replace(case.lower(),placeholder)
-            output.append((placeholder,case))
-    print("input",nameOnly)
-    print("SpecialCases",output)
+            nameOnly = nameOnly.replace(case.lower(), placeholder)
+            output.append((placeholder, case))
+    print("input", nameOnly)
+    print("SpecialCases", output)
     return (f"{nameOnly}.{split[1]}", output)
 
-def renameFile(inputName:str):
-    print("inputName",inputName)
+
+def renameFile(inputName: str):
+    print("inputName", inputName)
     specials = saveSpecialCases(inputName)
-    newName:str = str(specials[0])
+    newName: str = str(specials[0])
     for rule in rules:
         newName = rule(newName)
-        print("newName",newName)
+        print("newName", newName)
     for special in specials[1]:
-        newName = newName.replace(special[0],special[1])
-        print("newNameWithSpecials",newName)
+        newName = newName.replace(special[0], special[1])
+        print("newNameWithSpecials", newName)
     os.rename(inputName, newName)
+
 
 def renameAll():
     names = os.listdir()
@@ -91,9 +104,11 @@ def renameAll():
         if name != ".git" and name != ".vscode" and name != "@file-rename.py":
             renameFile(name)
 
+
 def main(argv):
     if len(argv) == 1:
         renameAll()
+
 
 if __name__ == '__main__':
     import sys
